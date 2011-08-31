d52b0071b90d30eca12cc11702de72e8b2dc80ae
Francesco Marassi <urcoilbisurco@gmail.com>
2011-08-31 16:36:59 +0200

    removed useless code

--- server.js
+++ server.js
@@ -34,33 +34,5 @@ app.get('/', function(req, res){
 
 
 app.listen(4000);
-/*var server = http.createServer(function(request, response) {
-	response.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
-console.log("request accepted");
-	response.end('_testcb(\'{"message": "Hello world!"}\')');
-//	response.write(md(string));
 
-});  // serves template.html to new clients
-
-
-server.listen(process.env.PORT || 4000);*/
-
-
-/*var io = require('socket.io').listen(server);
-
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-});
-
-io.sockets.on('connection', function(client) {
-	io.sockets.send(wallText);  // sends the current wall text to new clients
-
-	client.on('message', function(message) { // called when a change is received from a client
-		wallText = message;  // the new wall text is saved in the wallText variable
-		client.broadcast.send(wallText);  // the new wall text is sent to all OTHER clients
-	});
-});*/
 
